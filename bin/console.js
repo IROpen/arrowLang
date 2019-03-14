e860214fdb3b5086053d2fb3a290735c06133bf7
IROpen <hamid.6.11.79@gmail.com>
2019-03-14 15:40:36 +0330

    add run option in cli

--- bin/console.js
+++ bin/console.js
@@ -5,22 +5,34 @@ const program = require('commander');
 const { promisify } = require('util');
 const readFile = promisify(require('fs').readFile);
 const writeFile = promisify(require('fs').writeFile);
-
+const { spawn } = require('child_process');
 var scriptFile = "";
+
+process.on('unhandledRejection', up => { throw up });
 
 program
     .version(require('../package').version)
     .arguments('<file>').action(file=>(scriptFile=file))
     .option('--env-path <path>','path to env.js file')
+    .option('--run','run result now')
     .parse(process.argv);
 
 program.envPath = program.envPath || "arrowlang/std/env";
 
-if (scriptFile !== ""){
-    if (scriptFile.substr(-4) !== '.far'){
-        scriptFile += '.far';
-    }
-    readFile(scriptFile).then(data=>{
+function runAtMe(file){
+    //console.log(file);
+    let prog = spawn('node',[file]);
+    process.stdin.pipe(prog.stdin);
+    prog.stdout.pipe(process.stdout);
+    return prog;
+}
+
+async function main(){
+    if (scriptFile !== ""){
+        if (scriptFile.substr(-4) !== '.far'){
+            scriptFile += '.far';
+        }
+        let data = await readFile(scriptFile);
         //console.log(data.toString());
         let spliter = (s) => {
             let ar = [];
@@ -41,24 +53,25 @@ if (scriptFile !== ""){
             ar.push(t);
             return ar;
         }
-	    data = spliter(data.toString());
+        data = spliter(data.toString());
         //console.log(data);
         let outputFile = `
-            const { enviroment , IoMonad } = require('${program.envPath}');
+            const { enviroment , IoMonad , eventSystem } = require('${program.envPath}');
             module.exports.arrow = {f:[],pat:[],typ:[]};
             var f = module.exports.arrow.f;
             var pat = module.exports.arrow.pat;
             var typ = module.exports.arrow.typ;
-            
         `;
-	    for (let i = 0; i < data.length ; i++){
-		//console.log(data[i]);
+        for (let i = 0; i < data.length ; i++){
+        //console.log(data[i]);
             let res = itp.eval(data[i]);
             outputFile += res + '\n';
         }
         scriptFile = scriptFile.substring(0,scriptFile.length - 3);
         scriptFile+="js";
-        writeFile(scriptFile,outputFile);
-    });
-    
+        await writeFile(scriptFile,outputFile);
+        if (program.run) runAtMe(scriptFile);
+    }
 }
+
+main();
